ddfc9f79e9b170fa8275e47ae163736a791c05e8
mardabar <mossman1769@gmail.com>
2025-12-07 20:30:20 -0600

    pow and sqrt work now

--- src/Calculator.java
+++ src/Calculator.java
@@ -25,6 +25,11 @@ public class Calculator {
     private JButton a5Button;
     private JButton a2Button;
     private JButton a0Button;
+    private JButton button4;
+    private JButton button5;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
 
     double a, b, result;
     String op;
@@ -42,7 +47,7 @@ public class Calculator {
         // 00 button
         a00Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a00Button.getText());
             }
         });
@@ -50,7 +55,7 @@ public class Calculator {
         // 0 button
         a0Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a0Button.getText());
             }
         });
@@ -65,7 +70,7 @@ public class Calculator {
         // 2 button
         a2Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a2Button.getText());
             }
         });
@@ -73,7 +78,7 @@ public class Calculator {
         // 3 button
         a3Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a3Button.getText());
             }
         });
@@ -81,7 +86,7 @@ public class Calculator {
         // 4 button
         a4Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a4Button.getText());
             }
         });
@@ -89,7 +94,7 @@ public class Calculator {
         // 5 button
         a5Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a5Button.getText());
             }
         });
@@ -97,7 +102,7 @@ public class Calculator {
         // 6 button
         a6Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a6Button.getText());
             }
         });
@@ -113,7 +118,7 @@ public class Calculator {
         // 8 button
         a8Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a8Button.getText());
             }
         });
@@ -121,18 +126,16 @@ public class Calculator {
         // 9 button
         a9Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 txtDisplay.setText(txtDisplay.getText() + a9Button.getText());
             }
         });
 
 
-
-
         // + button
         button6.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
 //                String currentText = txtDisplay.getText();
 //                if(currentText.isEmpty()){
 //                    return;
@@ -146,7 +149,7 @@ public class Calculator {
         // - button
         button11.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 a = Double.parseDouble(txtDisplay.getText());
                 op = "-";
                 txtDisplay.setText("");
@@ -156,9 +159,9 @@ public class Calculator {
         // x button
         button3.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 String currentText = txtDisplay.getText();
-                if(currentText.isEmpty()){
+                if (currentText.isEmpty()) {
                     return;
                 }
                 a = Double.parseDouble(txtDisplay.getText());
@@ -170,10 +173,30 @@ public class Calculator {
         // ÷ button
         button2.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 a = Double.parseDouble(txtDisplay.getText());
                 op = "/";
                 txtDisplay.setText("");
+            }
+        });
+
+        // ^ button
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a = Double.parseDouble(txtDisplay.getText());
+                op = "^";
+                txtDisplay.setText(a + "^");
+            }
+        });
+
+        // sqrt button
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                a = Double.parseDouble(txtDisplay.getText());
+                op = "√";
+                txtDisplay.setText("√" + a);
             }
         });
 
@@ -181,13 +204,13 @@ public class Calculator {
         button16.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(txtDisplay.getText().contains(".")){
+                if (txtDisplay.getText().contains(".")) {
                     double num = Double.parseDouble(txtDisplay.getText());
-                    num = num*-1;
+                    num = num * -1;
                     txtDisplay.setText(String.valueOf(num));
                 } else {
                     long NUM = Long.parseLong(txtDisplay.getText());
-                    NUM = NUM*-1;
+                    NUM = NUM * -1;
                     txtDisplay.setText(String.valueOf(NUM));
                 }
             }
@@ -197,7 +220,7 @@ public class Calculator {
         button13.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(!txtDisplay.getText().contains(".")){
+                if (!txtDisplay.getText().contains(".")) {
                     txtDisplay.setText(txtDisplay.getText() + button13.getText());
                 }
             }
@@ -206,11 +229,10 @@ public class Calculator {
         // back button
         button17.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 String backspace = null;
 
-                if(txtDisplay.getText().length() > 0)
-                {
+                if (txtDisplay.getText().length() > 0) {
                     StringBuilder strB = new StringBuilder(txtDisplay.getText());
                     strB.deleteCharAt(txtDisplay.getText().length() - 1);
                     backspace = String.valueOf(strB);
@@ -223,29 +245,80 @@ public class Calculator {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //This makes the current text a list
+                String fullExpression = txtDisplay.getText();
+
+
                 b = Double.parseDouble(txtDisplay.getText());
 
-               if (op == "+"){
-                   result = a+b;
-                   txtDisplay.setText(String.valueOf(result));
-               } else if (op == "-"){
-                   result = a-b;
-                   txtDisplay.setText(String.valueOf(result));
-               } else if (op == "x"){
-                   result = a*b;
-                   txtDisplay.setText(String.valueOf(result));
-               } else if (op == "/"){
-                   result = a/b;
-                   txtDisplay.setText(String.valueOf(result));
-               }
+                if (op == "+") {
+                    result = a + b;
+                    txtDisplay.setText(String.valueOf(result));
+                } else if (op == "-") {
+                    result = a - b;
+                    txtDisplay.setText(String.valueOf(result));
+                } else if (op == "x") {
+                    result = a * b;
+                    txtDisplay.setText(String.valueOf(result));
+                } else if (op == "/") {
+                    if (b == 0) {
+                        txtDisplay.setText("Error");
+                    } else {
+                        result = a / b;
+                        txtDisplay.setText(String.valueOf(result));
+                    }
+                }
+
+
+                // pow func
+                if (op.equals("^")) {
+                    String[] parts = fullExpression.split("\\^");
+
+                    if (parts.length == 2 && !parts[1].isEmpty()) {
+                        try {
+                            b = Double.parseDouble(parts[1]);
+                            result = Math.pow(a, b);
+                            txtDisplay.setText(String.valueOf(result));
+                        } catch (NumberFormatException ex) {
+                            txtDisplay.setText("Error");
+                        }
+                    } else {
+                        txtDisplay.setText("Error: Missing exponent");
+                    }
+                    return;
+                }
+
+                // sqrt funct
+                if (op.equals("√")) {
+                    String[] parts = fullExpression.split("\\√");
+                    if (parts.length == 2 && !parts[1].isEmpty()) {
+                        try {
+                            b = Double.parseDouble(parts[1]);
+                            result = Math.sqrt(a);
+                            txtDisplay.setText(String.valueOf(result));
+                        } catch (NumberFormatException ex) {
+                            txtDisplay.setText("Error");
+                        }
+                        return;
+                    } else {
+                        txtDisplay.setText("Error: Missing root");
+                    }
+                    return;
+                }
 
 
             }
         });
 
+        // √ button
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-
+            }
+        });
     }
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
